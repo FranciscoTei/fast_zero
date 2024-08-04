e8c8d844234b73305b71f68f78db2812587f172a
FranciscoTei <francisco2mix@gmail.com>
2024-08-04 13:49:44 -0300

    implementando os endpoints de tarefas

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,12 +2,13 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 
-from fast_zero.routers import auth, users
+from fast_zero.routers import auth, todo, users
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(todo.router)
 
 
 @app.get('/')
